704eeef198e2eb842ec1e6ad5e7a550f12cc33e7
Luis Alejandro <luisalejandrordc@gmail.com>
2026-04-18 13:56:15 -0500

    Add Data-Driven-Insights/5/5.cpp solution

--- Data-Driven-Insights/5/5.cpp
+++ Data-Driven-Insights/5/5.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int maxSubArray(const vector<int> &nums) {
-  int current_sum = nums[0];
-  int max_sum = nums[0];
+// int maxSubarray(const vector<int> &nums) {
+//   int current_sum = nums[0];
+//   int max_sum = nums[0];
+//   for (int i = 1; i < nums.size(); i++) {
+//     current_sum = max(nums[i], current_sum + nums[i]);
+//     max_sum = max(max_sum, current_sum);
+//   }
+//   return max_sum;
+// }
 
+int maxSubarray(const vector<int> &nums) {
+  int curSum = nums[0];
+  int maxSum = nums[0];
   for (int i = 1; i < nums.size(); i++) {
-    current_sum = max(nums[i], current_sum + nums[i]);
-    max_sum = max(max_sum, current_sum);
+    curSum = curSum > 0 ? curSum + nums[i] : nums[i];
+    maxSum = max(maxSum, curSum);
   }
-
-  return max_sum;
+  return maxSum;
 }
 
 int main() {
-  cout << maxSubArray({-2, 1, -3, 4, -1, 2, 1, -5, 1});
+  cout << "Solution: " << maxSubarray({-2, 1, -3, 4, -1, 2, 1, -5, 1}) << endl;
   return 0;
 }
